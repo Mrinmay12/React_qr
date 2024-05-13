4ad4d7589f9ac8c2b31a44ac5ecc62e3ec20cbda
Mrinmay12 <88670421+Mrinmay12@users.noreply.github.com>
2024-05-13 10:41:30 +0530

    Update Qrcode.js

--- src/Qrcode.js
+++ src/Qrcode.js
@@ -1,138 +1,58 @@
-import "./index.css";
-import { useState } from "react";
-import {QrReader} from "react-qr-reader";
+import React, { useState } from 'react'
+import QRCode from 'qrcode.react';
+import {QrReader} from 'react-qr-reader';
+export default function Qrcode() {
 
-
-const Qrcode = () => {
-  const [code, setCode] = useState(null);
-  const [showDialog, setDiaglog] = useState(false);
-  const [processing, setProcessing] = useState(false);
-  const [precScan, setPrecScan] = useState("");
   const [selected, setSelected] = useState("environment");
-  const [errorMessage, setErrorMessage] = useState(null);
+        const handleScan = data => {
+          if (data) {
+            // Redirect to the scanned URL
+            window.location.href = "https://coderapp-eta.vercel.app/";
+          }
+        }
+    
+        const handleError = err => {
+          console.error(err);
+        }
 
+        const [value,setValue]=useState("1")
 
-
-  const handleScan = async (scanData) => {
-    console.log(`loaded data data`, scanData);
-    if (scanData && scanData !== "" && !showDialog && !processing) {
-      console.log(`loaded >>>`, scanData);
-      // setPrecScan(scanData);
-   
-    }
-  };
-  const handleError = (err) => {
-    console.error(err);
-  };
+        const handleOpen=(e)=>{
+            setValue(e)
+        }
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>
-        Last Scan:{precScan}
-        {selected}
-      </h2>
-      
-      {showDialog && (
-        <div className="dialog">
-          <div className="dialog-content">
-            <div className="close">
-              <button
-                onClick={() => {
-                  setCode(null);
-                  setErrorMessage(null);
-                  setDiaglog(false);
-                  setProcessing(false);
-                }}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </button>
-            </div>
-            {errorMessage && (
-              <div className="errorMessage">
-                <h2>{errorMessage}</h2>
-              </div>
-            )}
-            {code && (
-              <div className="description">
-                <h4 className="title">Scan Result</h4>
-                <div className="detail detail-first-child">
-                  <h6 className="detail-header">Matricule :</h6>
-                  <h6 className="detail-content green">{code.text}</h6>
-                </div>
-                <div className="detail">
-                  <h6 className="detail-header">Identité :</h6>
-                  <h6 className="detail-content">{code.identite}</h6>
-                </div>
-                <div className="detail">
-                  <h6 className="detail-header">Pomotion :</h6>
-                  <h6 className="detail-content">{code.promotion}</h6>
-                </div>
-                <div className="detail">
-                  <h6 className="detail-header">Année Academique :</h6>
-                  <h6 className="detail-content">{code.annee}</h6>
-                </div>
-                <div className="detail">
-                  <h6 className="detail-header">Total payé :</h6>
-                  <h6 className="detail-content red">
-                    {code.frais} (USD,dollars americains)
-                  </h6>
-                </div>
-                <div className="detail">
-                  <h6 className="detail-header">Total prévu :</h6>
-                  <h6 className="detail-content red">
-                    {code.total} (USD,dollars americains)
-                  </h6>
-                </div>
-                <div className="detail">
-                  <h6 className="detail-header">Reste à payer :</h6>
-                  <h6 className="detail-content red">
-                    {code.total - code.frais} (USD,dollars americains)
-                  </h6>
-                </div>
-                <div className="detail">
-                  <h6 className="detail-header">Votre Situation :</h6>
-                  <h6
-                    className={
-                      code.total <= code.frais
-                        ? `detail-content green`
-                        : "detail-content red small"
-                    }
-                  >
-                    {code.total <= code.frais
-                      ? "Eligible"
-                      : "Vous etes en retard de payement"}
-                  </h6>
-                </div>
-              </div>
-            )}
-          </div>
+    <div>
+        <div style={{ display:"flex",flexDirection:"row" ,justifyContent:"space-between"}}>
+            <p onClick={()=>handleOpen("1")} style={{cursor:"pointer" ,color:value==="1"?"red":"black"}}>
+                your qr 
+            </p>
+            <p style={{ paddingLeft:"49px",cursor:"pointer",color:value==="2"?"red":"black" }} onClick={()=>handleOpen("2")}>
+                scanned qr 
+            </p>
         </div>
-      )}
-      {/* {code && <h2>{code.text}</h2>} */}
-      {!showDialog && !processing && (
-        <QrReader
-          facingMode={selected}
-          delay={500}
-          onError={handleError}
-          onScan={handleScan}
-          // chooseDeviceId={()=>selected}
-          style={{ width: "200px", heigth: "100px" }}
-        />
-      )}
-    </div>
-  );
-};
+        {value==="1" ?(
 
-export default Qrcode;
+<QRCode value="https://coderapp-eta.vercel.app/" />
+        ):(
+            <>
+            <p>scanned</p>
+<QrReader
+      onError={handleError}
+      onScan={handleScan}
+      delay={500}
+      // style={{ width: '100%' }}
+      key="environment"
+  constraints={{ facingMode: 'environment' }}
+      style={{ width: "200px", heigth: "100px" }}
+     
+    />
+
+</>
+        )}
+
+
+
+
+    </div>
+  )
+}
